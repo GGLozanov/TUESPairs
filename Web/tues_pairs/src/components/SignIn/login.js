ae0ce66cdbc0abc3b84aae849059054a14042f59
marianbitsov23 <marianbitsov23@gmail.com>
2020-03-16 00:07:54 +0200

    [HOTFIX] Fixed Login page major bug!

--- Web/tues_pairs/src/components/SignIn/login.js
+++ Web/tues_pairs/src/components/SignIn/login.js
@@ -2,88 +2,80 @@ import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 import { compose } from 'recompose';
 
-import { PasswordForgetLink } from '../PasswordForget/passwordforget';
 import { SignUpLink } from '../SignUp/register';
 import { withFirebase } from '../Firebase';
 import * as ROUTES from '../../constants/routes';
 
 const SignInPage = () => (
-    <div>
-        <h1>SignIn</h1>
-        <SignInForm />
-        <PasswordForgetLink />
-        <SignUpLink />
-    </div>
+  <div>
+    <h1>SignIn</h1>
+    <SignInForm />
+    <SignUpLink />
+  </div>
 );
 
 const INITIAL_STATE = {
-    email: '',
-    password: '',
-    error: null,
+  email: '',
+  password: '',
+  error: null,
 };
 
-class SignInFromBase extends Component {
-    constructor(props) {
-        super(props);
+class SignInFormBase extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { ...INITIAL_STATE };
+  }
 
-        this.state = { ...INITIAL_STATE};
-    }
+  onSubmit = event => {
+    const { email, password } = this.state;
+    this.props.firebase
+      .doSignInWithEmailAndPassword(email, password)
+      .then(() => {
+        this.setState({ ...INITIAL_STATE });
+        this.props.history.push(ROUTES.HOME);
+      })
+      .catch(error => {
+        this.setState({ error });
+      });
+    event.preventDefault();
+  };
 
-    onSubmit = event => {
-        const { email, password } = this.state;
+  onChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
 
-        this.props.firebase
-            .doSignInWithEmailPassword(email, password)
-            .then(authUser => {
-                this.setState({ ...INITIAL_STATE });
-                this.props.history.push(ROUTES.HOME);
-            })
-            .catch(error => {
-                this.setState({ error });
-            });
-            
-        event.preventDefault();
-    };
-
-    onChange = event => {
-        this.setState({ [event.target.name]: event.target.value });
-    };
-
-    render() {
-        const { email, password, error } = this.state;
-
-        const isInvalid = password === '' || email === '';
-
-        return(
-            <form onSubmit={this.onSubmit}>
-                <input
-                    name="email"
-                    value={email}
-                    onChange={this.onChange}
-                    type="email"
-                    placeholder="Enter your email address"
-                />
-                <input
-                    name="password"
-                    value={password}
-                    onChange={this.onChange}
-                    type="password"
-                    placeholder="Enter your password"
-                />
-                <button disabled={isInvalid} type="submit">
-                    Sign In
-                </button>
-
-                {error && <p>{error.message}</p>}
-            </form>
-        );
-    }
+  render() {
+    const { email, password, error } = this.state;
+    const isInvalid = password === '' || email === '';
+    return (
+      <form onSubmit={this.onSubmit}>
+        <input
+          name="email"
+          value={email}
+          onChange={this.onChange}
+          type="text"
+          placeholder="Email Address"
+        />
+        <input
+          name="password"
+          value={password}
+          onChange={this.onChange}
+          type="password"
+          placeholder="Password"
+        />
+        <button disabled={isInvalid} type="submit">
+          Sign In
+        </button>
+        {error && <p>{error.message}</p>}
+      </form>
+    );
+  }
 }
 
 const SignInForm = compose(
-    withRouter,
-    withFirebase,
-)(SignInFromBase);
+  withRouter,
+  withFirebase,
+)(SignInFormBase);
 
 export default SignInPage;
 
