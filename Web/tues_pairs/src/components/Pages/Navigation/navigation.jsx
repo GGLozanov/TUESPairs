1d740c2f33407ebbef055d76dafeef96bf8da6d4
marianbitsov23 <marianbitsov23@gmail.com>
2020-04-02 04:20:20 +0300

    [Update] Added navbar icons

--- Web/tues_pairs/src/components/Pages/Navigation/navigation.jsx
+++ Web/tues_pairs/src/components/Pages/Navigation/navigation.jsx
@@ -4,6 +4,9 @@ import { Link } from 'react-router-dom';
 import SignOutButton from '../SignOut/signout.jsx';
 import * as ROUTES from '../../../constants/routes';
 import { AuthUserContext } from '../../Authentication';
+import PeopleIcon from '@material-ui/icons/People';
+import ChatIcon from '@material-ui/icons/Chat';
+import AssignmentIndIcon from '@material-ui/icons/AssignmentInd';
 import './style.scss';
 
 const Navigation = ({ authUser }) => (
@@ -19,9 +22,15 @@ const Navigation = ({ authUser }) => (
 const NavigationLogged = () => (
   <div className="links">
         <img src="https://bonitaselfdefense.com/wp-content/uploads/2017/04/default-image.jpg" alt="TUESPairs logo"></img>
-        <Link to={ROUTES.CHAT}>Chat</Link>
-        <Link to={ROUTES.HOME}>Home</Link>
-        <Link to={ROUTES.ACCOUNT}>Account</Link>
+        <Link to={ROUTES.CHAT}>
+          <ChatIcon fontSize="large"></ChatIcon>
+        </Link>
+        <Link to={ROUTES.HOME}>
+          <PeopleIcon fontSize="large"></PeopleIcon>
+        </Link>
+        <Link to={ROUTES.ACCOUNT}>
+        <AssignmentIndIcon fontSize="large"></AssignmentIndIcon>
+        </Link>
         <SignOutButton />
   </div>
 );
